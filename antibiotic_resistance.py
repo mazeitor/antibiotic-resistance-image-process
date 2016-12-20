2f202de63740dd2d656873f1ce02449663761311
mazeitor <mazeitor@gmail.com>
2016-12-20 15:05:58 +0000

    Added metadata

--- antibiotic_resistance.py
+++ antibiotic_resistance.py
@@ -1,3 +1,31 @@
+'''
+Copyright 2016, Oriol Mazariegos Canellas <oriol.mazariegos@gmail.com> 
+ 
+This file is part of the ARP application.
+ARP is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ARP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more denode_bs.
+You should have received a copy of the GNU General Public License
+along with ARP.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+'''
+Created on 01 August 2016
+@author: oriol mazariegos
+@copyright: Copyright 2016, ARP
+@credits: ["Oriol Mazariegos"]
+@license: GPL
+@version: 1.0.0
+@maintainer: oriol mazariegos
+@email: oriol.mazariegos@gmail.com
+@status: beta
+'''
+
 import argparse
 import cv2
 import math
